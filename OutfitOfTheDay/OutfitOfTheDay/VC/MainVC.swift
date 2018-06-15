259581c9c34835c1201247c16b2a0acc73d7588f
Clover <duwjdtmd91@gmail.com>
2018-06-16 00:02:06 +0900

    파일 재 업로드

---
+++ OutfitOfTheDay/OutfitOfTheDay/VC/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  MainVC.swift
+//  OutfitOfTheDay
+//
+//  Created by Jung seoung Yeo on 2018. 6. 13..
+//  Copyright © 2018년 LinSaeng. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: BaseVC {
+//    
+//    let mainView = MainView()
+//    
+//    override func setupView() {
+//        super.setupView()
+//        view = mainView
+//        NetWork.get("\(OOTD.NETWORK.URLPATH)")
+//    }
+//    
+//    
+}
