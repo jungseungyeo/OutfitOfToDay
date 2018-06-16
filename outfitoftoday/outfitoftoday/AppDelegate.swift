f04e2493f5d994dc2ec746f82dd4c3636d5df25d
Clover <duwjdtmd91@gmail.com>
2018-06-17 00:54:11 +0900

    project setting

--- outfitoftoday/outfitoftoday/AppDelegate.swift
+++ outfitoftoday/outfitoftoday/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let mainCollectionView = MainCollectionView(collectionViewLayout: layout)
+        window?.rootViewController = mainCollectionView
         return true
     }
 
