e4bbd98e70f4856adbe1999d3b24e6640fd72711
Clover <duwjdtmd91@gmail.com>
2018-06-16 15:40:36 +0900

    reupload

---
+++ OutfitOfTheDay/OutfitOfTheDay/Common/Network.swift
@@ -0,0 +1,21 @@
+//
+//  Network.swift
+//  OutfitOfTheDay
+//
+//  Created by Jung seoung Yeo on 2018. 6. 13..
+//  Copyright © 2018년 LinSaeng. All rights reserved.
+//
+
+import Alamofire
+import SwiftyJSON
+
+class NetWork {
+    static func get(_ urlPath: String, parameters: [String: Any] = [:]) {
+        Alamofire.request(urlPath).responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                print(swiftyJsonVar)
+            }
+        }
+    }
+}
