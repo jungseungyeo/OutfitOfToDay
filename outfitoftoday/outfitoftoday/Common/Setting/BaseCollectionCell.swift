5c117207a755bd6a28de6ccb482fae095227a709
Clover <duwjdtmd91@gmail.com>
2018-06-17 23:44:16 +0900

    첫 화면 설정 폰트 미적용

---
+++ outfitoftoday/outfitoftoday/Common/Setting/BaseCollectionCell.swift
@@ -0,0 +1,25 @@
+//
+//  BaseCollectionCell.swift
+//  outfitoftoday
+//
+//  Created by Jung seoung Yeo on 2018. 6. 17..
+//  Copyright © 2018년 LinSaeng. All rights reserved.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    func setupView() {
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
