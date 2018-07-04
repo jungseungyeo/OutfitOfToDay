72bf6f2bc3da2a5723b9de4c194877539582f5cd
Clover <duwjdtmd91@gmail.com>
2018-07-04 16:52:48 +0900

    make animationobject #9

--- outfitoftoday/outfitoftodayTests/outfitoftodayTests.swift
+++ outfitoftoday/outfitoftodayTests/outfitoftodayTests.swift
@@ -19,11 +19,10 @@ class outfitoftodayTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    
+   
     func test_makeAnimationObject() {
         let animationObject = WeatherAnimationViewController()
-        XCTAssertNotNil(animationObject)
+        XCTAssertNotNil(animationView)
     }
     
 }
