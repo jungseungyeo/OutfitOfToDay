490df5cb9f072b90ee0b136651c8e2eb98f1ced9
Clover <duwjdtmd91@gmail.com>
2018-07-04 15:26:18 +0900

    Rain 객체 생성 #9

---
+++ outfitoftoday/outfitoftoday/Animation/RainVC.swift
@@ -0,0 +1,14 @@
+//
+//  RainVC.swift
+//  outfitoftoday
+//
+//  Created by Jung seoung Yeo on 2018. 7. 4..
+//  Copyright © 2018년 LinSaeng. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class RainVC: BaseVC {
+    
+}
