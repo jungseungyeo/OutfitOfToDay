371711b271f91122f479c99a24f7c139a467a2e2
Clover <duwjdtmd91@gmail.com>
2018-07-04 18:02:05 +0900

    graph 영역 표시

--- outfitoftoday/outfitoftoday/View/SecondPage/WeatherGraph.swift
+++ outfitoftoday/outfitoftoday/View/SecondPage/WeatherGraph.swift
@@ -8,68 +8,28 @@
 
 import UIKit
 import SnapKit
+import Then
 
 class WeatherGraph: BaseView {
     
-    private var timeText = UILabel().then {
-        $0.text = ""
-        $0.textColor = .black
-        $0.font = .systemFont(ofSize: 10)
-    }
-    
-    private let scrollView = UIScrollView()
-    private let wrapper = UIView()
-    
+    private let graph: UIView = UIView()
     
     override func setupView() {
         super.setupView()
         
+        graph.backgroundColor = .red
+        
         self.snp.makeConstraints { make -> Void in
             make.height.equalTo(144)
-            make.width.equalTo(338)
-        }
-        makeTimeText()
-        addSubview(scrollView)
-        
-        scrollView.addSubview(wrapper)
-        wrapper.addSubview(timeText)
-        
-        scrollView.snp.makeConstraints { make in
-            make.left.equalToSuperview()
-            make.top.equalTo(100)
-            make.width.equalToSuperview()
+            make.width.equalTo(344)
         }
         
-        wrapper.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-            make.height.equalToSuperview()
-            make.left.equalToSuperview()
+        addSubViews(graph)
+        
+        graph.snp.makeConstraints { make -> Void in
+            make.top.bottom.left.right.equalTo(self)
         }
         
-        timeText.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-    }
-    
-    private func makeTimeText() {
-        
-        for index in 0 ... 24 {
-            var text = ""
-            if index == 0 {
-                text = timeText.text! + "0\(index)시"
-                timeText.text = text
-            }else {
-                 text = timeText.text! + "\(index)시"
-                timeText.text = text
-            }
-            
-            if 24 != index {
-                text = timeText.text! + "       "
-                timeText.text = text
-                
-            }
-        }
     }
     
 }
