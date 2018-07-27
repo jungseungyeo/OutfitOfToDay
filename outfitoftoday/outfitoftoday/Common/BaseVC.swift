fb307c21619a193467615c061b58c98cd269229d
Clover <duwjdtmd91@gmail.com>
2018-07-28 08:38:35 +0900

    생성: BaseVC, BaseView

---
+++ outfitoftoday/outfitoftoday/Common/BaseVC.swift
@@ -0,0 +1,30 @@
+//
+//  BaseVC.swift
+//  outfitoftoday
+//
+//  Created by Jung seoung Yeo on 2018. 7. 27..
+//  Copyright © 2018년 LinSaeng. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+    
+    // 1
+    internal func initAPI() {
+    }
+    
+    // 2
+    internal func setup() {
+    }
+    
+    // 3
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // 4
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
---
+++ outfitoftoday/outfitoftoday/Common/BaseView.swift
@@ -0,0 +1,26 @@
+//
+//  BaseView.swift
+//  outfitoftoday
+//
+//  Created by Jung seoung Yeo on 2018. 7. 27..
+//  Copyright © 2018년 LinSaeng. All rights reserved.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    internal func setup() {
+    }
+    
+}
